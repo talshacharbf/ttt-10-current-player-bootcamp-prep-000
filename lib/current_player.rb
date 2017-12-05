d16defec9d74ae36235535cc151e6a0580d6f7d2
tal shachar <tal.shachar@buzzfeed.com>
2017-12-05 22:37:38 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,8 +3,8 @@ counter = 0
 def turn_count (board)
   board.each do |turn|
   if turn = ("X" or "O")
-     counter += 1
-  else counter += 0
+     counter =+ 1
+  else counter =+ 0
   end
   return counter
 end
