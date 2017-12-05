2e402afff2ed395319c9db173b4b974fc465d8db
tal shachar <tal.shachar@buzzfeed.com>
2017-12-05 22:51:06 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ board = []
 def turn_count (board)
   counter = 0
   board.each do |turn|
-  if turn == ("X" or "O")
+  if turn == "X" || token == "O"
      counter =+ 1
   else counter =+ 0
   return counter
